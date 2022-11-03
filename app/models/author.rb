a52e42152a9984152c3b739d49f27c8008ac1a68
Ashton MacKenzie <pikeminnow@gmail.com>
2022-11-03 17:26:50 -0400

    commit tree 1e2f661fbb1c3143caa2d5d9f40e25ab7dc2c095

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,4 @@
 class Author < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :phone_number, presence: true, length: {is: 10}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,40 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: {
+         maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "is not included in the list"}
+
+    validate :clickbait_enough
+
+def clickbait_enough
+clickbait_name_formats = ["Won't Believe", "Secret", "Top", "Guess"]
+
+clickbaity = clickbait_name_formats.map { |name| title && title.include?(name)
+}
+
+if !clickbaity.include?(true)
+    errors.add(:title, "must be clickbait")
+end
+
+
+end
+
+
+
+    # validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "Must be Fiction or Non-Fiction"}
+
+    #this didn't work the way I wanted it to     
+    # validate :category_must_be_fiction_or_non
+
+    # def category_must_be_fiction_or_non
+    #     unless category == "Fiction" || category == "Non-Fiction"
+    #             errors.add(:category, "Must be Fiction or Non-Fiction")
+    #     end
+        
+    #     # if category.present? && (category != "Fiction" || category != "Non-Fiction")
+    #     #     errors.add(:category, "Must be Fiction or Non-Fiction")
+    #     # end
+    # end
+
 end
